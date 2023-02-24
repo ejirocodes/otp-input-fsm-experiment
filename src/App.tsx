f83645193619e0d67b6b4ea1578df6963e800062
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2023-02-24 14:16:23 +0100

    feat: hook input to machine states

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { useMachine } from "@zag-js/react";
+import { machine } from "./machine";
 
+const inputs = [...Array.from({ length: 4 }).keys()];
 function App() {
-  const inputs = Array.from({ length: 4 });
+  const [state, send] = useMachine(machine);
+
+  const { focusedIndex, value } = state.context;
+  const { type } = state.event;
+
   return (
     <div className="App">
+      <pre>{JSON.stringify({ value, focusedIndex, event: type })}</pre>
       <div data-part="container">
         <label htmlFor="">Enter verification</label>
         <div data-part="input-group">
-          <input type="text" data-part="input" />
+          {inputs.map((index) => (
+            <input
+              data-part="input"
+              key={index}
+              value={value[index]}
+              onChange={(event) => {
+                const { value } = event.target;
+                send({ type: "INPUT", index, value });
+              }}
+              onFocus={() => {
+                send({ type: "FOCUS", index });
+              }}
+              onBlur={() => {
+                send({ type: "BLUR", index });
+              }}
+              onKeyDown={(event) => {
+                if (event.key === "Backspace") {
+                  send({ type: "BACKSPACE", index });
+                }
+              }}
+              onPaste={(event) => {
+                const value = event.clipboardData.getData("text/plain");
+                send({ type: "PASTE", value, index });
+              }}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/machine.ts
+++ src/machine.ts
@@ -1,5 +1,6 @@
 import { createMachine } from "@zag-js/core";
 
+const FOCUSED_INDEX = -1;
 // state
 type MachineState = {
   value: "idle" | "focused";
@@ -11,38 +12,50 @@ type MachineContext = {
   focusedIndex: number;
 };
 
-export const machine = createMachine<MachineContext, MachineState>({
-  id: "pin-input",
-  context: {
-    value: [],
-    focusedIndex: -1,
-  },
-  states: {
-    idle: {
-      on: {
-        FOCUS: {
-          target: "focused",
-          actions: ["setFocusedIndex"],
+export const machine = createMachine<MachineContext, MachineState>(
+  {
+    id: "pin-input",
+    context: {
+      value: [],
+      focusedIndex: FOCUSED_INDEX,
+    },
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          FOCUS: {
+            target: "focused",
+            actions: ["setFocusedIndex"],
+          },
+        },
+      },
+      focused: {
+        on: {
+          BLUR: {
+            target: "idle",
+            actions: ["clearFocusedIndex"],
+          },
+          INPUT: {
+            actions: ["setFocusedValue", "focusedNextInput"],
+          },
+          BACKSPACE: {
+            actions: ["clearFocusedValue", "focusPreviousInput"],
+          },
+          PASTE: {
+            actions: ["setPastedValue", "focusedLastEmptyInput"],
+          },
         },
       },
     },
-    focused: {
-      on: {
-        BLUR: {
-          target: "idle",
-          actions: ["clearFocusedIndex"],
-        },
-        INPUT: {
-          actions: ["setFocusedValue", "focusedNextInput"],
-        },
-        BACKSPACE: {
-          actions: ["clearFocusedValue", "focusPreviousInput"],
-        },
-        PASTE: {
-          actions: ["setPastedValue", "focusedLastEmptyInput"],
-        },
+  },
+  {
+    actions: {
+      setFocusedIndex(context, event) {
+        context.focusedIndex = event.index;
+      },
+      clearFocusedIndex(context) {
+        context.focusedIndex = FOCUSED_INDEX;
       },
     },
-  },
-  initial: "idle",
-});
+  }
+);
